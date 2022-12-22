21811910a939108be3492f26c0c222ca0e4481c1
lucasfsf <lucasfreireri@gmail.com>
2022-12-22 06:52:28 -0300

    Pass all tests in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeValues) {
+    let indexOfValue;
+    for (let i = 0; i < removeValues.length; i++) {
+        indexOfValue = array.indexOf(removeValues[i]);
+        // skips loop iteration for non present values
+        if (indexOfValue === -1) {
+            continue;
+        }
+        array.splice(indexOfValue, 1); //removes one value starting on the index
+    }
+    return array;
 };
 
 // Do not edit below this line
