4a4629b4b151812bf29eb37409ad42ce879884ae
lucasfsf <lucasfreireri@gmail.com>
2022-12-22 07:21:50 -0300

    Pass all tests in 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if not divisible by 4, return false
+    if (year % 4 !== 0) {
+        return false;
+    // if divisible by 100 but not by 400, return false
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    // else return true
+    } else {
+        return true;
+    }
 };
 
 // Do not edit below this line
