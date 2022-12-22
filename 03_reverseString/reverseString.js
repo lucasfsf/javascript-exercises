b252e24c3c437a6268c2d012250a54fb894324bb
lucasfsf <lucasfreireri@gmail.com>
2022-12-22 06:36:37 -0300

    Pass all tests in 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedString = stringArray.reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
