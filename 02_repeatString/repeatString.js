5e4afc1d7f70bdf6f1c6c48ca5d104b99c165de0
lucasfsf <lucasfreireri@gmail.com>
2022-12-22 06:23:53 -0300

    Pass all tests in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    if (num < 0) {
+        newString = 'ERROR';
+    }
+    return newString;
 };
 
 // Do not edit below this line
