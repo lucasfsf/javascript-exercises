c2ff87d38dc74007a7b376dbb058f93877a4c762
lucasfsf <lucasfreireri@gmail.com>
2022-12-23 07:41:40 -0300

    Pass all tests in 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedString = stringArray.reverse().join("");
+    return reversedString;
+};
+
+const palindromes = function (string) {
+    // remove punctuation, wordbreaks and capitalization from string
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    let rawString =  string;
+    let rawLetters = rawString.split('');
+    let cleanLetters = rawLetters.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+      });
+    var cleanString = cleanLetters.join('').toLowerCase();
+    console.log(cleanString);
+    // created a new reversed string
+    let reversedString = reverseString(cleanString);  
+    // return the comparisson between then
+    return cleanString === reversedString;
 
 };
 
