760585abff1ecce16487967ff6575c131ce13417
lucasfsf <lucasfreireri@gmail.com>
2022-12-23 07:09:48 -0300

    Implement multiply function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(array) {
 	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
 const power = function() {
