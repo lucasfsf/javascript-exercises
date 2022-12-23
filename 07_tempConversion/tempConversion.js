ec83362b5d871751b67bd94a755ea3242ba3cc51
lucasfsf <lucasfreireri@gmail.com>
2022-12-23 06:54:48 -0300

    Pass all test in 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const roundToDecimal = (number, decimalPlaces) => Math.round(number*(10**decimalPlaces))/10**decimalPlaces;
+
+const convertToCelsius = function(temperature) {
+  // formula for conversion = (x - 32) x 5/9
+  let newTemperature = (temperature - 32) * 5/9;
+  return roundToDecimal(newTemperature, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // formula for conversion = (x . 9/5 + 32)
+  let newTemperature = (temperature * 9/5 + 32);
+  return roundToDecimal(newTemperature, 1);
 };
 
 // Do not edit below this line
